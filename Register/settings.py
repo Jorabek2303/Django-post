54f308f16836e0c435fe0b858be8602fb104704b
ibadov <ibadovjurabek@gmail.com>
2024-03-20 13:48:49 +0500

    database

--- Register/settings.py
+++ Register/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import os
 import environs
 import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,9 +70,17 @@ WSGI_APPLICATION = 'Register.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
-}
+if 'DATABASE_URL' in env('SECRET_KEY'):
+    DATABASES = {
+        'default': dj_database_url.parse(env('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
