e6e6059444462e0c521420e49045ed0722416015
ibadov <ibadovjurabek@gmail.com>
2024-04-05 18:20:26 +0500

    yangilik

--- Register/settings.py
+++ Register/settings.py
@@ -71,10 +71,7 @@ WSGI_APPLICATION = 'Register.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'default': dj_database_url.parse(env('DATABASE_URL'))
     }
 
 
